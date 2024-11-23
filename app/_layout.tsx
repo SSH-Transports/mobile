4adc08316be113fed1a2eb56a684804deea09bc5
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-11-23 20:04:11 -0300

    feat: Adding webview feature to app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,11 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import WebView from "react-native-webview";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -46,14 +45,21 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+   <SafeAreaView style={{flex: 1}}>
+     <WebView
+       source={{ uri: 'http://localhost:3000' }}
+       style={styles.webview}
+     />
+   </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
